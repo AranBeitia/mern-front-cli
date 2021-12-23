2298d585bcf0a77c74729395dfd9a858a06f97cb
Yaiser <yaiserar@gmail.com>
2021-12-23 10:53:31 +0100

    feat firebase settings and login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-import Home from './ui/views/pages/Home/Home'
-import Login from './ui/views/pages/Login'
+import Home from "./ui/views/pages/Home/Home";
+import Login from "./ui/views/pages/Login";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/login" element={<Login />} />
-		</Routes>
-	)
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/ui/views/pages/Login/Login.js
+++ src/ui/views/pages/Login/Login.js
@@ -1,5 +1,62 @@
+import React, { useState, useRef } from "react";
+import { Form, Card, Button, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../../../firebase";
+
 function Login() {
-	return <h1>hello login</h1>
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setError("");
+      setLoading(true);
+      await auth.signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      );
+      history("/");
+    } catch (error) {
+      setError("Failed to login");
+    }
+  }
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">SignUp</Link>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
