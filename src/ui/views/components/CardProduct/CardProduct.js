05aee4cb4a918208ce130cb7e0f21d8c725a8389
AranBeitia <arantza.beitia@gmail.com>
2021-12-23 13:39:33 +0100

    get products

--- src/ui/views/components/CardProduct/CardProduct.js
+++ src/ui/views/components/CardProduct/CardProduct.js
@@ -1,64 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import clientAxios from '../../../../config/axios'
+
 import Card from 'react-bootstrap/Card'
 import kitty from '../../../assets/img/hello-kitty.jpg'
 
 function CardProduct() {
+  const [products, setProducts] = useState([])
+
+  const consultAPI = async () => {
+    const consultProducts = await clientAxios.get('/books')
+    setProducts(consultProducts.data.data)
+  }
+
+  useEffect(() => {
+    consultAPI()
+  }, [])
+
   return (
     <>
-      <Card>
-        <Card.Img variant="top" src={kitty} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={kitty} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={kitty} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={kitty} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
+      {products ? (
+        products.map((product) => (
+          <Card>
+            <Card.Img variant="top" src={kitty} />
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        ))
+      ) : (
+        <p>No products</p>
+      )}
     </>
   )
 }
