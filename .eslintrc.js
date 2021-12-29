9c8407bc49ed78405e9f7c6c676886ba4a74fb8c
AranBeitia <arantza.beitia@gmail.com>
2021-12-29 13:44:06 +0100

    update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,36 @@
 const packageJson = require('./package.json')
-
 const devDependencies = Object.keys(packageJson.devDependencies || {})
+
 module.exports = {
   env: {
     commonjs: true,
-
     es2021: true,
-
     node: true,
-
     jest: true,
   },
   extends: [
     'eslint:recommended',
-
     'plugin:jest/recommended',
-
     'prettier',
-
     'prettier/prettier',
-
     'plugin:node/recommended',
   ],
-
   parser: '@babel/eslint-parser',
-
   parserOptions: {
     ecmaVersion: 12,
-
     sourceType: 'module',
-
     requireConfigFile: false,
   },
-
   plugins: ['jest'],
-
   rules: {
     'prefer-destructuring': 'off',
-
     'object-shorthand': 'off',
-
     'arrow-body-style': 'off',
-
     'no-underscore-dangle': 'off',
-
     'react/react-in-jsx-scope': 'off',
-
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-
     'node/no-unpublished-require': [
       'error',
-
       {
         allowModules: devDependencies,
       },
