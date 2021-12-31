d102b856eb918a32541d89c9127cd361cfd4eb25
AranBeitia <arantza.beitia@gmail.com>
2021-12-31 18:25:32 +0100

    new products form

--- src/ui/views/pages/Product/ProductNew.js
+++ src/ui/views/pages/Product/ProductNew.js
@@ -1,19 +1,64 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import clientAxios from '../../../../config/axios'
 
 import Header from '../../components/Layout/Header'
 import AdminNav from '../../components/Layout/AdminNav'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Swal from 'sweetalert2'
 
 function ProductNew() {
+  let navigate = useNavigate()
   const [product, setProduct] = useState({
     title: '',
+    description: '',
     price: '',
     stock: '',
   })
   const [file, setFile] = useState('')
+
+  const handleProduct = (e) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleFile = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('title', product.title)
+    formData.append('description', product.description)
+    formData.append('price', product.price)
+    formData.append('stock', product.stock)
+    formData.append('images', file)
+    console.log(product)
+
+    try {
+      const res = await clientAxios.post('/products', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      if (res.status === 200) {
+        Swal.fire('Product added correctly', res.data.message, 'success')
+        navigate('/products')
+      }
+    } catch (error) {
+      Swal.fire({
+        type: 'error',
+        title: 'An error occurred',
+        text: 'Try again',
+      })
+    }
+  }
   return (
     <>
       <Header title={'Administration'} />
@@ -21,32 +66,57 @@ function ProductNew() {
         <AdminNav />
         <main>
           <h2>new product</h2>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <div className="row mb-3">
               <Form.Group controlId="title" className="col">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter title" />
+                <Form.Control
+                  type="text"
+                  name="title"
+                  placeholder="Enter title"
+                  onChange={handleProduct}
+                />
               </Form.Group>
 
               <Form.Group controlId="description" className="col">
                 <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Enter description" />
+                <Form.Control
+                  type="text"
+                  name="description"
+                  placeholder="Enter description"
+                  onChange={handleProduct}
+                />
               </Form.Group>
             </div>
             <div className="row mb-3">
               <Form.Group className="col" controlId="price">
                 <Form.Label>Price</Form.Label>
-                <Form.Control type="number" placeholder="1234" />
+                <Form.Control
+                  type="number"
+                  name="price"
+                  placeholder="1234"
+                  onChange={handleProduct}
+                />
               </Form.Group>
 
               <Form.Group className="col" controlId="stock">
                 <Form.Label>Stock</Form.Label>
-                <Form.Control type="number" placeholder="32" />
+                <Form.Control
+                  type="number"
+                  name="stock"
+                  placeholder="32"
+                  onChange={handleProduct}
+                />
               </Form.Group>
             </div>
 
-            <Form.Group className="mb-3" id="image">
-              <Form.Control type="file" label="Upload image" />
+            <Form.Group className="mb-3" id="images">
+              <Form.Control
+                type="file"
+                name="images"
+                label="Upload image"
+                onChange={handleFile}
+              />
             </Form.Group>
 
             <Button variant="info" type="submit">
