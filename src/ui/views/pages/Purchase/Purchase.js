732dbf1d9f4c6a6b5ddb3c2c5ce8d9849aa6cae5
Yaiser <yaiserar@gmail.com>
2022-01-11 05:08:59 +0100

    feat purchase page user data && 4 steps done

--- src/ui/views/pages/Purchase/Purchase.js
+++ src/ui/views/pages/Purchase/Purchase.js
@@ -1,16 +1,57 @@
 import React, {useState, useRef} from 'react'
 import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Header from '../../components/Layout/Header'
 import { useAuth } from '../../../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 
 export default function Purchase() {
+    const history = useNavigate()
     const { currentUser } = useAuth()
+    const [address, setAddress] = useState('')
+    const [town, setTown] = useState('')
+    const [state, setState] = useState('')
+    const [postCode, setPostCode] = useState('')
+    const [country, setCountry] = useState('')
+    const [cardNumber, setCardNumber] = useState('')
+    const [card, setCard] = useState('')
+    const [cvc, setCvc] = useState('')
+
+    const changeAddress = (e) => {
+        setAddress(e.target.value)
+    }
+
+    const changeTown = (e) => {
+        setTown(e.target.value)
+    }
+
+    const changeState = (e) => {
+        setState(e.target.value)
+    }
+
+    const changePostCode = (e) => {
+        setPostCode(e.target.value)
+    }
+
+    const changeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const changeCardNumber = (e) => {
+        setCardNumber(e.target.value)
+    }
+
+    const changeCard = (e) => {
+        setCard(e.target.value)
+    }
+
+    const changeCvc = (e) => {
+        setCvc(e.target.value)
+    }
+
     const [step, setStep] = useState(1)
-    const emailRef = useRef()
 
     let prevDisabled = false
     let nextDisabled = false
@@ -27,10 +68,39 @@ export default function Purchase() {
         e.preventDefault()
         e.stopPropagation()
         
-        const email = emailRef.current.value
 
+        //STEP 1 and 2 to database by patch find user by email in:
+        
+        
+        //STEP get from localStorage product data set the billing data to database by patch
+        //! currentUser._uid
+        
+        const values = {
+            address: address,
+            country: country,
+            postCode: postCode,
+            state: state,
+            town: town,
+            cardNumber: cardNumber,
+            cvc: cvc,
+            card: card
+        }
+        
         console.log(currentUser);
+
+        //Clean the state
+        setAddress('')
+        setCountry('')
+        setPostCode('')
+        setState('')
+        setTown('')
+        setCardNumber('')
+        setCvc('')
+        setCard('')
+
+        history('/')
     }
+
 
     return (
         <div>
@@ -45,47 +115,120 @@ export default function Purchase() {
             </div>
             <Form onSubmit={handleSubmit}>
             {step===1 && (<div> <h1>1.User address and shipping details</h1>
-                <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
+                <Form.Group id="address">
+                <Form.Label>Adress</Form.Label>
                 <Form.Control
-                type="email"
-                ref={emailRef}
+                type="text"
+                name="address"
+                value={address}
+                onChange={changeAddress}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="town">
+                <Form.Label>Town</Form.Label>
+                <Form.Control
+                type="text"
+                name="town"
+                value={town}
+                onChange={changeTown}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="state">
+                <Form.Label>State</Form.Label>
+                <Form.Control
+                type="text"
+                name="state"
+                value={state}
+                onChange={changeState}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="postCode">
+                <Form.Label>PostCode</Form.Label>
+                <Form.Control
+                type="text"
+                name="postCode"
+                value={postCode}
+                onChange={changePostCode}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="country">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                type="text"
+                name="country"
+                value={country}
+                onChange={changeCountry}
                 required
                 ></Form.Control>
                 </Form.Group>
                 </div>)
             }
             {step===2 && (<div> <h1> 2.Payment info</h1>
-                <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
+                <Form.Group id="card">
+                <Form.Label>Card Holder Fullname</Form.Label>
                 <Form.Control
-                type="email"
-                ref={emailRef}
+                type="text"
+                name="card"
+                value={card}
+                onChange={changeCard}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="cvc">
+                <Form.Label>CVC</Form.Label>
+                <Form.Control
+                type="text"
+                name="cvc"
+                value={cvc}
+                onChange={changeCvc}
+                required
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group id="cardNumber">
+                <Form.Label>Card Number</Form.Label>
+                <Form.Control
+                type="text"
+                name="cardNumber"
+                value={cardNumber}
+                onChange={changeCardNumber}
                 required
                 ></Form.Control>
                 </Form.Group>
                 </div>)
             }
             {step===3 && (<div> <h1>3.Review info</h1>
-                <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                ></Form.Control>
-                </Form.Group>
+                <p>Here the summary of the purchase with a get from the localstorage</p>
+                <div>
+                    {address}
+                </div>
+                <div>
+                    {town}
+                </div>
+                <div>
+                    {state}
+                </div>
+                <div>
+                    {postCode}
+                </div>
+                <div>
+                    {country}
+                </div>
+                <div>
+                    {card}
+                </div>
+                <div>
+                    {cvc}
+                </div>
+                <div>
+                    {cardNumber}
+                </div>
                 </div>)
             }
             {step===4 && (<div> <h1>4.Confirm checkout</h1>
-                <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                ></Form.Control>
-                </Form.Group>
                 <Button className="w-100" type="submit">Confirm Purchase</Button>
                 </div>)
             }
